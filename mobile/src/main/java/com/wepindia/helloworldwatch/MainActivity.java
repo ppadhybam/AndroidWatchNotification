b817b2e6abdde4cd0e66e6ae43bf5c3617b7e335
PriyabratP <ppadhybam@gmail.com>
2017-01-13 17:01:50 +0530

    Initial commit

---
+++ mobile/src/main/java/com/wepindia/helloworldwatch/MainActivity.java
@@ -0,0 +1,45 @@
+package com.wepindia.helloworldwatch;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_EVENT_ID = "EXTRA_EVENT_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void notifyNow(View view) {
+        int notificationId = 001;
+        // Build intent for notification content
+        Intent viewIntent = new Intent(this, ViewEventActivity.class);
+        viewIntent.putExtra(EXTRA_EVENT_ID, 123);
+        PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Hey title")
+                        .setContentText("hey text")
+                        .setContentIntent(viewPendingIntent);
+
+        NotificationCompat.WearableExtender wearableExtender =
+                new NotificationCompat.WearableExtender()
+                        .setHintHideIcon(true);
+
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+}
